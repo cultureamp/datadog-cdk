5739bc7ddf204515cf5cbe0668408280f390bdbf
James Telfer <792299+jamestelfer@users.noreply.github.com>
2023-05-27 22:05:43 +1000

    test: ensure SLO references are handled

--- __tests__/slo.test.ts
+++ __tests__/slo.test.ts
@@ -59,4 +59,44 @@ describe("Given an SLO construct", () => {
       expect(template).toMatchSnapshot()
     })
   })
+
+  describe("When I reference fields", () => {
+    const stack = testStack()
+
+    const slo = new CfnSlo(stack, "TestSlo", {
+      name: "TestSlo",
+      thresholds: [
+        {
+          target: 100,
+          timeframe: ThresholdTimeframe.VALUE_7D,
+          targetDisplay: "98.0",
+        },
+      ],
+      type: CfnSloPropsType.METRIC,
+    })
+
+    new cdk.CfnResource(stack, "TestResource", {
+      type: "Test::Resource",
+      properties: {
+        idref: slo.ref,
+        id: slo.attrId,
+        deleted: slo.attrDeleted,
+      },
+    })
+
+    test("Then the references appear in the synthesised output", () => {
+      const template = Template.fromStack(stack)
+      template.hasResourceProperties("Test::Resource", {
+        deleted: {
+          "Fn::GetAtt": ["TestSlo", "Deleted"],
+        },
+        id: {
+          "Fn::GetAtt": ["TestSlo", "Id"],
+        },
+        idref: {
+          Ref: "TestSlo",
+        },
+      })
+    })
+  })
 })
